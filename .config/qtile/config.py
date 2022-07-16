ec8733ffd36360b5a0c75b5d2575b4d3a61c39ee
ItzDilan <99371498+ItzDilan@users.noreply.github.com>
2022-07-16 09:56:06 -0600

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -81,13 +81,59 @@ keys = [
 
 ### COLORS ###
 
-colors = ["#151515","#404040",
-          "#e9e9e9","#000000",
-          "#3f5965","#ffffff",
+# Dracula
+#colors = ["#282a36","#4c566a",
+#          "#e9e9e9","#292d3e",
+#          "#A77AC4","#ffffff",
+#          "#727072","#ff6188",
+#          "#ff5555","#A77AC4",
+#          "#7197E7","#ffb86c",
+#          "#232f35","#4c566a"]
+
+# Gruvbox dark
+colors = ["#282828","#5c5545",
+          "#e9e9e9","#ebdbb2",
+          "#458588","#ffffff",
           "#727072","#ff6188",
-          "#8fa1b3","#a3be8c",
-          "#b48ead","#bf616a",
-          "#232f35","#414753"]
+          "#689d6a","#d79921",
+          "#cc241d","#458588",
+          "#232f35","#42644f"]
+
+# Monokai
+#colors = ["#2D2A2E","#727072",
+#          "#e9e9e9","#151515",
+#          "#fc9867","#ffffff",
+#          "#727072","#ff6188",
+#          "#a9dc76","#ffd866",
+#          "#ab9df2","#fc9867",
+#          "#232f35","#727072"]
+
+# Nord
+#colors = ["#2E3440","#4c566a",
+#          "#e9e9e9","#212121",
+#          "#81a1c1","#ffffff",
+#          "#727072","#ff6188",
+#          "#81a1c1","#88c0d0",
+#          "#a3be8c","#ebcb8b",
+#          "#232f35","#4c566a"]
+
+# Solarized Dark
+#colors = ["#002b36","#6a6a6a",
+#          "#e9e9e9","#212121",
+#          "#268BD2","#ffffff",
+#          "#727072","#ff6188",
+#          "#2AA198","#D33682",
+#          "#DC322F","#869900",
+#          "#232f35","#694c6a"]
+
+# Material Ocean
+#colors = ["#0f101a","#4c566a",
+#          "#e9e9e9","#0f101a",
+#          "#a151d3","#ffffff",
+#          "#727072","#ff6188",
+#          "#a151d3","#F07178",
+#          "#fb9f7f","#ffd47e",
+#          "#232f35","0f101a"]
 
 ### WORKSPACES ###
 
@@ -174,29 +220,13 @@ screens = [
         widget.WindowName(
             font = "Liberation Sans Bold",
             fontsize = 12,
-            foreground = colors[8],
-            max_chars = 70,
+            foreground = colors[4],
+            max_chars = 50,
             ),
-        widget.TextBox(
+       widget.TextBox(
             "",
             fontsize = 44,
-            foreground = colors[13],
             background = colors[0],
-            padding = -3,
-            ),
-        widget.Systray(
-            background = colors[13],
-            padding = 4,
-            ),
-        widget.Sep(
-            padding = 4,
-            foreground = colors[13],
-            background = colors[13],
-            ),
-        widget.TextBox(
-            "",
-            fontsize = 44,
-            background = colors[13],
             foreground = colors[11],
             padding = -3,
             ),
@@ -229,7 +259,7 @@ screens = [
             "Qtile ",
             background = colors[10],
             foreground = colors[3],
-            mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' -e nvim /home/user/.config/qtile/settings/keys.py')},
+            mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' -e nvim /home/user/.config/qtile/config.py')},
             ),
         widget.TextBox(
             "",
@@ -263,11 +293,27 @@ screens = [
             background = colors[8],
             format="  %d/%m/%Y   %I:%M %p ",
             ),
+        widget.TextBox(
+            "",
+            fontsize = 44,
+            foreground = colors[0],
+            background = colors[8],
+            padding = -3,
+            ),
+        widget.Systray(
+            background = colors[0],
+            padding = 6,
+            ),
+        widget.Sep(
+            padding = 4,
+            foreground = colors[0],
+            background = colors[0],
+            ), 
             ],
         31,
         background = colors[0],
         margin = 0,
-        opacity = 0.90,
+        opacity = 0.95,
         ),
         ),
         ]
