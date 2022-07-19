35d47832a02e205b7376f499dae6b9d3a88c3911
Dilan Rojas <99371498+dilanrojas@users.noreply.github.com>
2022-07-18 21:34:07 -0600

    Update colors.py

--- .config/qtile/themes/MaterialOcean/colors.py
+++ .config/qtile/themes/MaterialOcean/colors.py
@@ -1,7 +1,7 @@
 ### COLORS ###
 
 # Material Ocean
-colors = ["#0f101a","#4c566a",
+colors = ["#0f101a","#414a5b",
           "#e9e9e9","#0f101a",
           "#a151d3","#ffffff",
           "#727072","#ff6188",
